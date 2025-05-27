adee2b597ee0036f0dc4fe8bcec48c75335986ed
kongduk <gongduk4960@naver.com>
2025-05-27 09:12:11 -0700

    feat(develop):깃허브 오류 해결

--- src/App/Route.tsx
+++ src/App/Route.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Profile from '../Pages/Profile';
 import EditProfile from '../Pages/EditProfile';
+import ChooseEmotion from '../Pages/ChooseEmotion';
 
 export const router = createBrowserRouter([
     // 여기에 라우트 정보를 추가하세요
+    {
+        path: "/emotions",
+        element: <ChooseEmotion />,
+    },
     {
         path: "/profile",
         element: <Profile />,
