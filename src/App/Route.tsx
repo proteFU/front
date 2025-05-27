fe96d2cd9cd2738640f9e414a7f1e37f92698204
kongduk <gongduk4960@naver.com>
2025-05-27 09:23:28 -0700

    feat(develop):임시 커밋

--- src/App/Route.tsx
+++ src/App/Route.tsx
@@ -1,20 +1,83 @@
 import { createBrowserRouter } from "react-router-dom";
 import Profile from '../Pages/Profile';
 import EditProfile from '../Pages/EditProfile';
+import MusicPlayer from '../Pages/MusicPlayer';
+import NotFound from '../Pages/NotFound';
+import MusicDetail from '../Pages/MusicDetail';
+import Home from '../Pages/Home';
 import ChooseEmotion from '../Pages/ChooseEmotion';
+import GNB from '../Shared/UI/GNB';
+import styled from "@emotion/styled";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SignUp";
+
+const PageContainer = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    padding-bottom: 80px;
+    position: relative;
+    z-index: 1;
+`;
+
+const GNBWrapper = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    margin-top: 100px;
+`;
+
+const withGNB = (Component: React.ComponentType) => {
+    return (
+        <>
+            <PageContainer>
+                <Component />
+            </PageContainer>
+            <GNBWrapper>
+                <GNB />
+            </GNBWrapper>
+        </>
+    );
+};
 
 export const router = createBrowserRouter([
-    // 여기에 라우트 정보를 추가하세요
     {
-        path: "/emotions",
-        element: <ChooseEmotion />,
+        path: "/",
+        element: withGNB(Home),
     },
     {
         path: "/profile",
-        element: <Profile />,
+        element: withGNB(Profile),
     },
     {
         path: "/profile/edit",
-        element: <EditProfile />,
-    }   
-]); 
+        element: withGNB(EditProfile),
+    },
+    {
+        path: "/music-player",
+        element: withGNB(MusicPlayer),
+    },
+    {
+        path: "/music-detail",
+        element: withGNB(MusicDetail),
+    },
+    {
+        path: "/choose-emotion",
+        element: withGNB(ChooseEmotion),
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    }
+]);
+
+export default router; 
